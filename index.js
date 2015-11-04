97c047a6b282599bd78ecc47216bb4e603f4e363
Ransom Briggs <rbriggs@heroku.com>
2015-11-04 16:15:53 -0600

    Fix buildpack typo to buildpacks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ exports.topics = [
   { name: 'info',        hidden: true, },
   { name: 'maintenance', description: 'manage maintenance mode for an app' },
   { name: 'stack',       description: 'manage the stack for an app' },
-  { name: 'buildpacks',  description: 'manage the buildpack for an app' },
+  { name: 'buildpacks',  description: 'manage the buildpacks for an app' },
 ];
 
 exports.commands = [
