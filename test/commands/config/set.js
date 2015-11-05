fddf2f16205448c0fc9586a5c0685afe300bd45f
dickeyxxx <jeff@dickeyxxx.com>
2015-11-04 16:19:26 -0800

    show set config vars in config:set

--- test/commands/config/set.js
+++ test/commands/config/set.js
@@ -10,8 +10,9 @@ describe('config:set', function() {
   it('sets a config var', function() {
     let api = nock('https://api.heroku.com:443')
       .patch('/apps/myapp/config-vars', {RACK_ENV: 'production'})
-      .reply(200);
+      .reply(200, {RACK_ENV: 'production', RAILS_ENV: 'production'});
     return cmd.run({app: 'myapp', args: ['RACK_ENV=production']})
+    .then(() => expect(cli.stdout).to.equal('RACK_ENV: production\n'))
     .then(() => expect(cli.stderr).to.equal('Setting config vars and restarting myapp... done\n'))
     .then(() => api.done());
   });
