3a103d00332edb0ba3be0ca3bd0cc19351e1af92
dickeyxxx <jeff@dickeyxxx.com>
2015-12-09 13:21:18 -0800

    added extended output

--- commands/drains/index.js
+++ commands/drains/index.js
@@ -4,12 +4,16 @@ let cli = require('heroku-cli-util');
 let co  = require('co');
 
 function* run (context, heroku) {
-  let drains = yield heroku.request({path: `/apps/${context.app}/log-drains`});
+  let path = `/apps/${context.app}/log-drains`;
+  if (context.flags.extended) path = path + '?extended=true';
+  let drains = yield heroku.request({path});
   if (context.flags.json) {
     cli.log(JSON.stringify(drains, null, 2));
   } else {
     drains.forEach(function (drain) {
-      cli.log(`${cli.color.cyan(drain.url)} (${cli.color.green(drain.token)})`);
+      let output = `${cli.color.cyan(drain.url)} (${cli.color.green(drain.token)})`;
+      if (drain.extended) output = output + ` drain_id=${drain.extended.drain_id}`;
+      cli.log(output);
     });
   }
 }
@@ -21,6 +25,7 @@ module.exports = {
   needsAuth: true,
   flags: [
     {name: 'json', description: 'output in json format'},
+    {name: 'extended', char: 'x', hidden: true},
   ],
   run: cli.command(co.wrap(run))
 };
