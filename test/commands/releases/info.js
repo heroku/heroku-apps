d7942b3030760af8a736f7495033998061c12fca
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-20 12:44:43 -0700

    fix tests

--- test/commands/releases/info.js
+++ test/commands/releases/info.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let cmd = commands.find(c => c.topic === 'releases' && c.command === 'info');
+let expect = require('unexpected');
 
 let d = new Date(2000, 1, 1);
 
@@ -10,15 +11,19 @@ describe('releases:info', function() {
   it('shows most recent release info', function() {
     let api = nock('https://api.heroku.com:443')
       .get('/apps/myapp/releases')
-      .reply(200, [{name: 'v10', descr: 'something changed', user: 'foo@foo.com', created_at: d}]);
+      .reply(200, [{version: 10}])
+      .get('/apps/myapp/releases/10')
+      .reply(200, {name: 'v10', descr: 'something changed', user: 'foo@foo.com', created_at: d, env: {foo: 'bar'}});
     return cmd.run({app: 'myapp', flags: {}, args: {}})
-    .then(() => expect(cli.stdout).to.equal(`=== Release v10
+    .then(() => expect(cli.stdout, 'to equal', `=== Release v10
 By:      foo@foo.com
 Change:  something changed
 When:    ${d.toISOString()}
 
+=== v10 Config vars
+foo: bar
 `))
-    .then(() => expect(cli.stderr).to.equal(''))
+    .then(() => expect(cli.stderr, 'to be empty'))
     .then(() => api.done());
   });
 
@@ -27,7 +32,7 @@ When:    ${d.toISOString()}
       .get('/apps/myapp/releases/10')
       .reply(200, {name: 'v10', descr: 'something changed', user: 'foo@foo.com', created_at: d, env: {foo: 'bar'}});
     return cmd.run({app: 'myapp', flags: {}, args: {release: 'v10'}})
-    .then(() => expect(cli.stdout).to.equal(`=== Release v10
+    .then(() => expect(cli.stdout, 'to equal', `=== Release v10
 By:      foo@foo.com
 Change:  something changed
 When:    ${d.toISOString()}
@@ -35,7 +40,7 @@ When:    ${d.toISOString()}
 === v10 Config vars
 foo: bar
 `))
-    .then(() => expect(cli.stderr).to.equal(''))
+    .then(() => expect(cli.stderr, 'to be empty'))
     .then(() => api.done());
   });
 
@@ -44,8 +49,8 @@ foo: bar
       .get('/apps/myapp/releases/10')
       .reply(200, {name: 'v10', descr: 'something changed', user: 'foo@foo.com', created_at: d, env: {foo: 'bar'}});
     return cmd.run({app: 'myapp', flags: {json: true}, args: {release: 'v10'}})
-    .then(() => expect(JSON.parse(cli.stdout).name).to.equal('v10'))
-    .then(() => expect(cli.stderr).to.equal(''))
+    .then(() => expect(JSON.parse(cli.stdout), 'to satisfy', {name: 'v10'}))
+    .then(() => expect(cli.stderr, 'to be empty'))
     .then(() => api.done());
   });
 });
