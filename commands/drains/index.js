9993475a8e6b913615e55c0e313879e1b0a73a72
Jeff Dickey <dickeyxxx@gmail.com>
2016-07-19 17:46:40 -0700

    fix drains call with paranoid apps
    Fixes https://github.com/heroku/heroku/issues/1982

--- commands/drains/index.js
+++ commands/drains/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-let cli = require('heroku-cli-util')
-let co = require('co')
+const cli = require('heroku-cli-util')
+const co = require('co')
 
 function styledDrain (id, name, drain) {
   let output = `${id} (${name})`
@@ -10,26 +10,28 @@ function styledDrain (id, name, drain) {
 }
 
 function * run (context, heroku) {
+  const zip = require('lodash.zip')
   const partition = require('lodash.partition')
 
   let path = `/apps/${context.app}/log-drains`
   if (context.flags.extended) path = path + '?extended=true'
-  let drains = yield heroku.request({path})
+  let drains = yield heroku.get(path)
   if (context.flags.json) {
     cli.styledJSON(drains)
   } else {
-    drains = partition(drains, 'addon')
-    if (drains[1].length > 0) {
+    let [addonDrains, appDrains] = partition(drains, 'addon')
+    let addons = zip(addonDrains, yield addonDrains.map(d => heroku.get(`/apps/${context.app}/addons/${d.addon.name}`)))
+    if (appDrains.length > 0) {
       cli.styledHeader('Drains')
-      drains[1].forEach((drain) => {
+      appDrains.forEach(drain => {
         styledDrain(drain.url, cli.color.green(drain.token), drain)
       })
     }
-    if (drains[0].length > 0) {
-      let addons = yield drains[0].map((d) => heroku.get(`/apps/${context.app}/addons/${d.addon.name}`))
+    if (addons.length > 0) {
       cli.styledHeader('Add-on Drains')
-      addons.forEach((addon, i) => {
-        styledDrain(cli.color.yellow(addon.plan.name), cli.color.green(addon.name), drains[0][i])
+      addons.forEach(d => {
+        let [drain, addon] = d
+        styledDrain(cli.color.yellow(addon.plan.name), cli.color.green(addon.name), drain)
       })
     }
   }
