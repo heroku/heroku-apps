d3badeab8f8d7d21c4452d9e8f1321f428459aff
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-09-30 16:13:53 -0700

    added autoscaling check to ps:scale

--- commands/ps/scale.js
+++ commands/ps/scale.js
@@ -22,19 +22,41 @@ function * run (context, heroku) {
     }))
   }
 
-  let changes = parse(context.args)
-  if (changes.length === 0) {
-    let formation = yield heroku.get(`/apps/${app}/formation`)
+  const displayFormation = co.wrap(function * () {
+    const formation = yield heroku.get(`/apps/${app}/formation`)
     if (formation.length === 0) throw emptyFormationErr(app)
     cli.log(formation.map((d) => `${d.type}=${d.quantity}:${d.size}`).sort().join(' '))
-  } else {
-    yield cli.action('Scaling dynos', {success: false}, co(function * () {
-      let formation = yield heroku.request({method: 'PATCH', path: `/apps/${app}/formation`, body: {updates: changes}})
+  })
+
+  const autoscalingCheck = co.wrap(function * () {
+    let monitors
+    try {
+      const appID = (yield heroku.get(`/apps/${app}`)).id // TODO: metrics api will eventually not require app ID
+      monitors = yield heroku.get(`/apps/${appID}/formation/web/monitors`, {host: 'api.metrics.herokai.com'})
+    } catch (err) { // skip autoscaling check if any http errors
+      cli.hush(err)
+      return
+    }
+    if (monitors.find(m => m.name === 'LATENCY' && m.action_type === 'scale' && m.is_active)) {
+      yield cli.confirmApp(app, context.flags.confirm, `This dyno is currently configured to autoscale.
+Manually changing the scale will disable autoscaling.
+Please enter the app name to confirm that you want to manually scale this app:`)
+    }
+  })
+
+  const setFormation = changes => {
+    return cli.action('Scaling dynos', {success: false}, co(function * () {
+      if (changes.find(c => c.type === 'web')) yield autoscalingCheck()
+      let formation = yield heroku.patch(`/apps/${app}/formation`, {body: {updates: changes}})
       let output = formation.filter((f) => changes.find((c) => c.type === f.type))
-        .map((d) => `${cli.color.green(d.type)} at ${d.quantity}:${d.size}`)
+        .map(d => `${cli.color.green(d.type)} at ${d.quantity}:${d.size}`)
       cli.action.done(`done, now running ${output.join(', ')}`)
     }))
   }
+
+  let changes = parse(context.args)
+  if (changes.length === 0) yield displayFormation()
+  else yield setFormation(changes)
 }
 
 let cmd = {
@@ -53,6 +75,9 @@ Examples:
   $ heroku ps:scale
   web=3:Standard-2X worker=1:Standard-1X
 `,
+  flags: [
+    {name: 'confirm', hasValue: true, hidden: true}
+  ],
   needsAuth: true,
   needsApp: true,
   run: cli.command(co.wrap(run))
