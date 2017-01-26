2688ccf12874a2f46a2a0af5327b9cd7ede37f7a
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-01-25 22:07:47 -0800

    wip

--- commands/config/index.js
+++ commands/config/index.js
@@ -1,34 +1,41 @@
 'use strict'
 
-const cli = require('heroku-cli-util')
-const co = require('co')
+const {Command, api, app} = require('heroku-command')
 
-function * run (context, heroku) {
-  const shellescape = require('shell-escape')
-  const forEach = require('lodash.foreach')
-  const mapKeys = require('lodash.mapkeys')
+class Config extends Command {
+  async run () {
+    // const shellescape = require('shell-escape')
+    // const forEach = require('lodash.foreach')
+    // const mapKeys = require('lodash.mapkeys')
 
-  let configVars = yield heroku.request({path: `/apps/${context.app}/config-vars`})
-  if (context.flags.shell) {
-    forEach(configVars, function (v, k) {
-      cli.log(`${k}=${shellescape([v])}`)
-    })
-  } else if (context.flags.json) {
-    cli.styledJSON(configVars)
-  } else {
-    cli.styledHeader(`${context.app} Config Vars`)
-    cli.styledObject(mapKeys(configVars, (_, k) => cli.color.configVar(k)))
+    console.log(this.app)
+    let configVars = await this.api.get(`/apps/${this.app}/config-vars`)
+    console.log(configVars)
+    // if (context.flags.shell) {
+    //   forEach(configVars, function (v, k) {
+    //     cli.log(`${k}=${shellescape([v])}`)
+    //   })
+    // } else if (context.flags.json) {
+    //   cli.styledJSON(configVars)
+    // } else {
+    //   cli.styledHeader(`${context.app} Config Vars`)
+    //   cli.styledObject(mapKeys(configVars, (_, k) => cli.color.configVar(k)))
+    // }
   }
 }
 
-module.exports = {
-  topic: 'config',
-  description: 'display the config vars for an app',
-  needsApp: true,
-  needsAuth: true,
-  flags: [
-    {name: 'shell', char: 's', description: 'output config vars in shell format'},
-    {name: 'json', description: 'output config vars in json format'}
-  ],
-  run: cli.command(co.wrap(run))
-}
+Config.topic = 'config'
+Config.description = 'display the config vars for an app'
+Config.mixins = [api(), app()]
+
+module.exports = Config
+
+// module.exports = {
+//   needsApp: true,
+//   needsAuth: true,
+//   flags: [
+//     {name: 'shell', char: 's', description: 'output config vars in shell format'},
+//     {name: 'json', description: 'output config vars in json format'}
+//   ],
+//   run: cli.command(co.wrap(run))
+// }
