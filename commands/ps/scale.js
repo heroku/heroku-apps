da302789ae789796d1c478bbc21a4295f92ecad5
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-01-27 09:27:14 -0800

    ps:scale

--- commands/ps/scale.js
+++ commands/ps/scale.js
@@ -1,46 +1,51 @@
 'use strict'
 
-let cli = require('heroku-cli-util')
-let co = require('co')
+const {Command, mixins} = require('heroku-command')
 
-let emptyFormationErr = (app) => {
-  return new Error(`No process types on ${cli.color.app(app)}.
+class Scale extends Command {
+  async run () {
+    const compact = require('lodash.compact')
+    let app = this.app
+
+    function parse (args) {
+      return compact(args.map((arg) => {
+        let change = arg.match(/^([a-zA-Z0-9_]+)([=+-]\d+)(?::([\w-]+))?$/)
+        if (!change) return
+        let quantity = change[2][0] === '=' ? change[2].substr(1) : change[2]
+        return {type: change[1], quantity, size: change[3]}
+      }))
+    }
+
+    let changes = parse(this.args)
+    if (changes.length === 0) {
+      let formation = await this.api.get(`/apps/${app}/formation`)
+      if (formation.length === 0) throw this.emptyFormationErr(app)
+      this.log(formation.map((d) => `${d.type}=${d.quantity}:${d.size}`).sort().join(' '))
+    } else {
+      let scaling = async function () {
+        let formation = await this.api.request({method: 'PATCH', path: `/apps/${app}/formation`, body: {updates: changes}})
+        let output = formation.filter((f) => changes.find((c) => c.type === f.type))
+          .map((d) => `${this.color.green(d.type)} at ${d.quantity}:${d.size}`)
+        this.action.done(`done, now running ${output.join(', ')}`)
+      }
+      await this.action('Scaling dynos', {success: false}, scaling.call(this))
+    }
+  }
+
+  emptyFormationErr (app) {
+    return new Error(`No process types on ${this.color.app(app)}.
 Upload a Procfile to add process types.
 https://devcenter.heroku.com/articles/procfile`)
-}
-
-function * run (context, heroku) {
-  const compact = require('lodash.compact')
-  let app = context.app
-
-  function parse (args) {
-    return compact(args.map((arg) => {
-      let change = arg.match(/^([a-zA-Z0-9_]+)([=+-]\d+)(?::([\w-]+))?$/)
-      if (!change) return
-      let quantity = change[2][0] === '=' ? change[2].substr(1) : change[2]
-      return {type: change[1], quantity, size: change[3]}
-    }))
   }
 
-  let changes = parse(context.args)
-  if (changes.length === 0) {
-    let formation = yield heroku.get(`/apps/${app}/formation`)
-    if (formation.length === 0) throw emptyFormationErr(app)
-    cli.log(formation.map((d) => `${d.type}=${d.quantity}:${d.size}`).sort().join(' '))
-  } else {
-    yield cli.action('Scaling dynos', {success: false}, co(function * () {
-      let formation = yield heroku.request({method: 'PATCH', path: `/apps/${app}/formation`, body: {updates: changes}})
-      let output = formation.filter((f) => changes.find((c) => c.type === f.type))
-        .map((d) => `${cli.color.green(d.type)} at ${d.quantity}:${d.size}`)
-      cli.action.done(`done, now running ${output.join(', ')}`)
-    }))
-  }
 }
 
-let cmd = {
-  variableArgs: true,
-  description: 'scale dyno quantity up or down',
-  help: `Appending a size (eg. web=2:Standard-2X) allows simultaneous scaling and resizing.
+Scale.topic = 'ps'
+Scale.command = 'scale'
+Scale.description = 'scale dyno quantity up or down'
+Scale.variableArgs = true
+Scale.aliases = ['ps:scale', 'dyno:scale', 'scale']
+Scale.help = `Appending a size (eg. web=2:Standard-2X) allows simultaneous scaling and resizing.
 
 Omitting any arguments will display the app's current dyno formation, in a
 format suitable for passing back into ps:scale.
@@ -52,14 +57,8 @@ Examples:
 
   $ heroku ps:scale
   web=3:Standard-2X worker=1:Standard-1X
-`,
-  needsAuth: true,
-  needsApp: true,
-  run: cli.command(co.wrap(run))
-}
+`
 
-module.exports = [
-  Object.assign({topic: 'ps', command: 'scale'}, cmd),
-  Object.assign({topic: 'dyno', command: 'scale'}, cmd),
-  Object.assign({topic: 'scale', hidden: true}, cmd)
-]
+mixins.api(Scale)
+mixins.app(Scale)
+module.exports = Scale
