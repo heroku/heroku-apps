2d6668fc5c509fa60269092eccdf27c6663100cd
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-01-22 13:34:19 -0800

    indicate locked apps

--- test/commands/apps/index.js
+++ test/commands/apps/index.js
@@ -12,6 +12,13 @@ let example = {
   region: {name: 'us'}
 }
 
+let lockedApp = {
+  name: 'locked-app',
+  owner: {email: 'foo@bar.com'},
+  region: {name: 'us'},
+  locked: true
+}
+
 let euApp = {
   name: 'example-eu',
   owner: {email: 'foo@bar.com'},
@@ -132,6 +139,20 @@ org-app-1   test-org@herokumanager.com
         expect(cli.stdout).to.equal(`=== foo@bar.com Apps
 example
 example-eu (eu)
+
+`)
+      })
+    })
+
+    it('shows locked apps', function () {
+      let mock = stubUserApps([example, euApp, lockedApp])
+      return apps.run({flags: {}, args: {}}).then(function () {
+        mock.done()
+        expect(cli.stderr).to.equal('')
+        expect(cli.stdout).to.equal(`=== foo@bar.com Apps
+example
+example-eu (eu)
+locked-app (locked)
 
 `)
       })
