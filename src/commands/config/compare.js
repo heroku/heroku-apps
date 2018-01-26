cae5e7a45ac6db460b7153a2cd30d18ab077db96
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2018-01-25 16:19:28 -0800

    add compare-and-set

---
+++ src/commands/config/compare.js
@@ -0,0 +1,64 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+let co = require('co')
+
+function * run (context, heroku) {
+  function lastRelease () {
+    return heroku.request({
+      method: 'GET',
+      partial: true,
+      path: `/apps/${context.app}/releases`,
+      headers: {Range: 'version ..; order=desc,max=1'}
+    }).then((releases) => releases[0])
+  }
+
+  let [key, value] = context.args.pair.split('=')
+
+  if (!key || !value) {
+    cli.error(`${cli.color.cyan(context.args.pair)} is invalid. Must be in the format ${cli.color.cyan('FOO=bar')}.`)
+  }
+
+  let configVars = yield heroku.request({path: `/apps/${context.app}/config-vars`})
+  let v = configVars[key]
+  if (v === undefined) {
+    cli.error(`${cli.color.cyan(context.args.key)} is invalid`)
+  } else {
+    if (v === context.flags.expected) {
+      let config = {}
+      config[key] = value
+      yield cli.action(
+        `Setting ${key} and restarting ${cli.color.app(context.app)}`,
+        {success: false},
+        co(function * () {
+          yield heroku.request({
+            method: 'patch',
+            path: `/apps/${context.app}/config-vars`,
+            body: config
+          })
+          let release = yield lastRelease()
+          cli.action.done(`done, ${cli.color.release('v' + release.version)}`)
+          cli.log(`${key}: ${value}`)
+        })
+      )
+    } else {
+      cli.error(`${cli.color.cyan(key)} value doesn't match as expected`)
+    }
+  }
+}
+
+module.exports = {
+  topic: 'config',
+  command: 'compare-and-set',
+  description: 'reset a config value if it matches the old',
+  help: `Example:
+
+    $ heroku heroku config:compare-and-set FOO=baz --expected bar
+    production
+ `,
+  args: [{name: 'pair'}],
+  flags: [{name: 'expected', char: 'e', description: 'key value expected', hasValue: true}],
+  needsApp: true,
+  needsAuth: true,
+  run: cli.command(co.wrap(run))
+}
